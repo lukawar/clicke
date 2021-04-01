2fe40b17a12c3aa257b114e5cce615d14c5c00a9
lukawar <lwargula@gmail.com>
2021-04-01 12:50:00 +0200

    Add actions

--- uclicker.pas
+++ uclicker.pas
@@ -13,9 +13,21 @@ type
   { TFClicker }
 
   TFClicker = class(TForm)
+    MenuItem3: TMenuItem;
+    StopApp: TAction;
+    DisplayForm: TAction;
     ActionList: TActionList;
+    MenuItem1: TMenuItem;
+    MenuItem2: TMenuItem;
+    MenuItem4: TMenuItem;
     PopupMenu1: TPopupMenu;
     TrayIcon: TTrayIcon;
+    procedure DisplayFormExecute(Sender: TObject);
+    procedure MenuItem1Click(Sender: TObject);
+    procedure FormShow(Sender: TObject);
+    procedure FormWindowStateChange(Sender: TObject);
+    procedure StopAppExecute(Sender: TObject);
+    procedure TrayIconClick(Sender: TObject);
   private
 
   public
@@ -28,6 +40,47 @@ var
 implementation
 
 {$R *.lfm}
+
+{ TFClicker }
+
+
+
+procedure TFClicker.FormShow(Sender: TObject);
+begin
+  FClicker.Hide;
+  FClicker.ShowInTaskBar:=stNever;
+end;
+
+procedure TFClicker.FormWindowStateChange(Sender: TObject);
+begin
+  if FClicker.WindowState=wsMinimized then begin
+      FClicker.WindowState:=wsNormal;
+      FClicker.Hide;
+      FClicker.ShowInTaskBar:=stNever;
+
+  end;
+end;
+
+procedure TFClicker.StopAppExecute(Sender: TObject);
+begin
+  Application.Terminate;
+end;
+
+procedure TFClicker.TrayIconClick(Sender: TObject);
+begin
+
+end;
+
+procedure TFClicker.MenuItem1Click(Sender: TObject);
+begin
+
+end;
+
+procedure TFClicker.DisplayFormExecute(Sender: TObject);
+begin
+  FClicker.Show;
+  FClicker.WindowState:=wsNormal;
+end;
 
 end.
 
